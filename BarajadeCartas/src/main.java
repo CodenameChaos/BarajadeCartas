23398a220408800dcfc6fe8a524028e34bf29d05
Guille-web69 <falivene@gmail.com>
2024-03-21 19:49:59 -0300

    Creo la baraja en el main

--- BarajadeCartas/src/main.java
+++ BarajadeCartas/src/main.java
@@ -8,7 +8,7 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Baraja baraja = new Baraja();
     }
     
 }
