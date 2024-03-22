548cf5a5fb6c77d7c3a2ff898787ee914fa41000
Guille-web69 <falivene@gmail.com>
2024-03-22 17:11:03 -0300

    Cambios en Baraja
    Realizamos public void barajar()
            Baraja baraja = new Baraja();
            baraja.crearBaraja();

--- BarajadeCartas/src/Baraja.java
+++ BarajadeCartas/src/Baraja.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,15 +15,19 @@ public class Baraja {
         crearBaraja();
     }
 
-    private void crearBaraja() {
+    public void crearBaraja() {
         String[] palos = {"Espadas", "Bastos", "Oros", "Copas"};
         for (String palo : palos) {
             for (int numero = 1; numero <= 12; numero++) {
                 if (numero != 8 && numero != 9) {
                     cartas.add(new Carta(numero, palo));
+//                    System.out.println(cartas.toString());
                 }
             }
         }
     }
-
+ public void barajar(){
+     Collections.shuffle(cartas);
+ }
+ 
 }
